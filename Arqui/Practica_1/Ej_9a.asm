949c6c7a7a39268eb2839335c2ac053a8d64f5b8
CapitanaBanana <63492980+CapitanaBanana@users.noreply.github.com>
2022-08-29 16:43:53 -0300

    Update Ej_9a.asm

--- Arqui/Practica_1/Ej_9a.asm
+++ Arqui/Practica_1/Ej_9a.asm
@@ -16,3 +16,16 @@ push ax
 call ROTARIZQ
 hlt
 end
+~~~~~~~~~~~~~~~corregido~~~~~~~~~~~~~~~~~~~~~~
+org 1000h
+byt db 01101011b
+org 3000h
+ROTARIZQ:  add ah, ah
+  adc ah,0
+  mov byt, ah
+  ret
+org 2000h
+mov ah, byt
+call ROTARIZQ
+hlt
+end
